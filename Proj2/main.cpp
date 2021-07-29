8056f1b831e0176202d5db3d55bc5367a643e4b2
Ferwhatever <fenandomldev@gmail.com>
2021-07-28 23:28:27 -0700

    init

---
+++ Proj2/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int num{100};
+    cout << "Hello from Proj2 , enter your fav number" << endl;
+    cin >> num;
+    cout << " Your favorite number is: "<< num << endl;
+    return 0;
+}
